17ed39c05abd48832b31e3a4f66c875b7f3762bc
annmutemi <annshiru92@gmail.com>
2024-07-18 10:33:16 +0300

    Fetch lab

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  const books = fetch('https://anapioficeandfire.com/api/books')
+  .then((resp) => resp.json()).then((json) => renderBooks(json));
+  return books;
 }
-
+ 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
@@ -11,7 +13,7 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
-
+ 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
